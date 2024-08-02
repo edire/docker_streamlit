d9c3b25979d0d2de8df354bbe399c183702b5df6
edire <eric.di.re@gmail.com>
2024-08-02 15:35:37 -0700

    updated cron job to hard reset git branch

--- cron_git_update.sh
+++ cron_git_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /.env
 cd /git_repo
-git fetch --depth 1
-git merge origin/$GIT_BRANCH
+git fetch origin $GIT_BRANCH
+git reset --hard origin/$GIT_BRANCH
 /usr/local/bin/python -m pip install -r /git_repo/requirements.txt
 echo "Git Repo updated at $(date)"
