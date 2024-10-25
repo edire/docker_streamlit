b3ac966de177ff4e6cd20b3ed59563665b4e7bc7
edire <eric.di.re@gmail.com>
2024-10-25 13:48:01 -0700

    added known_host and config logic

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+mkdir -p ~/.ssh
 cp /volume/git_mm_docker /volume/git_mm_docker.pub ~/.ssh/
-echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/git_mm_docker" >> ~/.ssh/config
+# echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/git_mm_docker" >> ~/.ssh/config
+cat <<EOL >> ~/.ssh/config
+Host github.com
+    HostName github.com
+    User git
+    IdentityFile ~/.ssh/git_mm_docker
+EOL
+ssh-keyscan -t ed25519 github.com >> ~/.ssh/known_hosts
 git clone -b $GIT_BRANCH --depth 1 $GIT_REPO git_repo
 echo "GIT_BRANCH=$GIT_BRANCH" > /.env
 cd git_repo
